b7585d9687beb87160095b05d477653c800a55a4
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-26 21:02:34 +0530

    [Mahesh]Add:Enum Example

---
+++ EnumProblem/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumProblem
+{
+    internal class Employee
+    {
+
+        public enum DayofWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday, Sunday
+        }
+    }
+}
---
+++ EnumProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumProblem
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("*****Welcome to Enum Problem*******");
+            DayOfWeek wd;
+            wd = DayOfWeek.Monday;
+            Console.WriteLine("Day is  " + wd);
+        }
+    }
+}
